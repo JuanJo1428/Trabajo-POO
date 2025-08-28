cea0678747c82fa1c5fa08d8a49fdc0738b67743
JuanJo1428 <juvasquezco@unal.edu.co>
2025-08-28 12:50:04 -0500

    Ejercicio Resuelto 12.py

--- Ejercicio Resuelto 12.py
+++ Ejercicio Resuelto 12.py
@@ -16,17 +16,16 @@ class SR:
     def CalcularNeto(SalarioBruto, Retencion):
         SalarioN = SalarioBruto - Retencion
         return SalarioN
+
+
+HorasTrabajadas = 48 * 4
+SalarioBruto = SR.CalcularBruto(HorasTrabajadas)
+RetencionFuente = SR.CalcularRetencion(SalarioBruto)
+SalarioNeto = SR.CalcularNeto(SalarioBruto, RetencionFuente)
+
+print(f"El Salario Bruto del trabajador es de ${SalarioBruto}.")
+print(f"Pero  al tener una Retención en la fuente de ${RetencionFuente}, en realidad su Salario Neto es de ${SalarioNeto}.")
     
 
-def Valores():
-    
-    HorasTrabajadas = 48 * 4
-    SalarioBruto = SR.CalcularBruto(HorasTrabajadas)
-    RetencionFuente = SR.CalcularRetencion(SalarioBruto)
-    SalarioNeto = SR.CalcularNeto(SalarioBruto, RetencionFuente)
 
-    print(f"El Salario Bruto del trabajador es de ${SalarioBruto}.")
-    print(f"Pero  al tener una Retención en la fuente de ${RetencionFuente}, en realidad su Salario Neto es de ${SalarioNeto}.")
-    
-Valores()
 
